ac5a72bba5728b8d313aebb5616b9826f8752865
nacharya114 <nacharya114@gmail.com>
2017-10-14 07:32:46 -0400

    Database integration

--- HackGt2017/delta/models.py
+++ HackGt2017/delta/models.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+	username = models.CharField(max_length=200, default="username")
+	talk = models.NullBooleanField()
+	sleep = models.NullBooleanField()
+	drink = models.NullBooleanField()
+	child = models.NullBooleanField()
+
+class Seat(models.Model):
+	number = models.IntegerField(default=0)
+	profile = models.ForeignKey(
+			'Profile',
+			on_delete=models.SET_NULL,
+			null=True
+		)
+
